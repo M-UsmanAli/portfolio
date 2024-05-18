946cbc635cfcf059f19627e2aacc4d32e982a725
M-UsmanAli <aliusmanmuhammad98@gmail.com>
2024-05-18 23:47:41 +0500

    created and designed Hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './Hero.css'
+import profile_img from '../../assets/profile_img.svg'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='hero'>
+        <img src={profile_img} alt=''/>
+        <h1><span>I am Muhammad Usman Ali,</span> full stack developer.</h1>
+        <p>As a dedicated React.js and Next.js application developer, I have played a pivotal role in creating cutting-edge and dynamic user interfaces. My expertise extends to the entire software development life cycle, from conceptualization and design to implementation and deployment.</p>
+        <div className="hero-action">
+            <div className="hero-connect">Connect With Me</div>
+            <div className="hero-resume">My Resume</div>
+        </div>
+    </div>
   )
 }
 
