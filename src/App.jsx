59bf60d7fd631d63c9087dd6e6a33673a7593556
M-UsmanAli <aliusmanmuhammad98@gmail.com>
2024-05-19 01:15:54 +0500

    Added and designed about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
+import AboutSection from './components/AboutSection/AboutSection'
+
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <AboutSection/>
     </div>
   )
 }
---
+++ src/components/AboutSection/AboutSection.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import "./AboutSection.css";
+import theme_pattern from "../../assets/theme_pattern.svg";
+import profile_img from "../../assets/about_profile.svg";
+
+const AboutSection = () => {
+  return (
+    <div className="about">
+      <div className="about-title">
+        <h1>About Me</h1>
+        <img src={theme_pattern} alt="" />
+      </div>
+      <div className="about-sections">
+        <div className="about-left">
+          <img src={profile_img} alt="" />
+        </div>
+
+        <div className="about-right">
+          <div className="about-para">
+            <p>
+              I am a software engineer with expertise in React.js and Next.js application development. With a passion for creating innovative and user-friendly solutions.
+            </p>
+            <p>
+              My expertise extends to the entire software development lifecycle,
+              from conceptualization and design to implementation and
+              deployment.
+            </p>
+          </div>
+          <div className="about-skills">
+            <div className="about-skill"><p>HTML & CSS</p><hr style={{ width: "50%" }} /></div>
+            <div className="about-skill"><p>React JS</p><hr style={{ width: "70%" }} /></div>
+            <div className="about-skill"><p>JavaScript</p><hr style={{ width: "60%" }} /></div>
+            <div className="about-skill"><p>Next JS</p><hr style={{ width: "50%" }} /></div>
+          </div>
+        </div>
+      </div>
+      <div className="about-achievements">
+          <div className="about-achievement">
+            <h1>2+</h1>
+            <p>YEARS OF EXPREINCE</p>
+          </div>
+          <hr />
+          <div className="about-achievement">
+            <h1>30+</h1>
+            <p>PEOJECTS COMPLETED</p>
+          </div>
+          <hr />
+          <div className="about-achievement">
+            <h1>15+</h1>
+            <p>SATISFIED CLIENTS</p>
+          </div>
+        </div>
+    </div>
+  );
+};
+
+export default AboutSection;
