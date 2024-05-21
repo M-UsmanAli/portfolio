8840df9afdd39322e1fda52c27ef7555bf792277
M-UsmanAli <aliusmanmuhammad98@gmail.com>
2024-05-22 00:22:37 +0500

    adding the navlink to the buttons too

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Hero.css'
 import profile_img from '../../assets/profile_img.svg'
-
+import AnchorLink from "react-anchor-link-smooth-scroll";
 const Hero = () => {
   return (
     <div className='hero' id='home'>
@@ -9,7 +9,7 @@ const Hero = () => {
         <h1><span>I am Muhammad Usman Ali,</span> full stack developer.</h1>
         <p>As a dedicated React.js and Next.js application developer, I have played a pivotal role in creating cutting-edge and dynamic user interfaces. My expertise extends to the entire software development life cycle, from conceptualization and design to implementation and deployment.</p>
         <div className="hero-action">
-            <div className="hero-connect">Connect With Me</div>
+            <div className="hero-connect"><AnchorLink className="anchor-link" offset={50} href="#contact">Connect With Me</AnchorLink></div>
             <div className="hero-resume">My Resume</div>
         </div>
     </div>
