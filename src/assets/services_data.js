596c80c5ebac7513c03148b2d9a3ae24d9a68d9b
M-UsmanAli <aliusmanmuhammad98@gmail.com>
2024-05-28 00:23:53 +0500

    SEO content of Services

--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -1,33 +1,33 @@
 const Services_Data = [
     {
         s_no:"01",
-        s_name:"Web design",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Web Development",
+        s_desc:"Expert web development with React.js, Next.js, HTML, CSS, Tailwind, & PHP for robust websites."
     },
     {
         s_no:"02",
         s_name:"Graphics design",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"Creating impactful and visually stunning designs to enhance your brand's visual identity."
     },
     {
         s_no:"03",
-        s_name:"Social media",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Web Applications",
+        s_desc:"High-performance web applications with React.js, Next.js, and PHP for seamless user experiences."
     },
     {
         s_no:"04",
         s_name:"App design",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"Crafting intuitive, innovative mobile and web applications with a focus on exceptional user experience."
     },
     {
         s_no:"05",
-        s_name:"Digital marketing",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"WordPress",
+        s_desc:"Custom WordPress themes and plugins, optimized performance, and enhanced functionality."
     },
     {
         s_no:"06",
-        s_name:"Content writing",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"UI/UX Design",
+        s_desc:"User-centered UI/UX designs to improve usability, accessibility, and user satisfaction."
     },
 ]
  
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
-import './Services.css'
-import theme_pattern from '../../assets/theme_pattern.svg'
 import Services_Data from '../../assets/services_data'
-import arrow_icon from '../../assets/arrow_icon.svg'
+import theme_pattern from '../../assets/theme_pattern.svg'
+import './Services.css'
 
 const Services = () => {
   return (
     <div className='services' id='services'>
         <div className="services-title">
             <h1>My Services</h1>
-            <img src={theme_pattern} alt="" />
+            <img src={theme_pattern} alt="theme_pattern" />
         </div>
         <div className="services-container">
             {Services_Data.map((service,index)=>{
@@ -17,10 +16,6 @@ const Services = () => {
                     <h3>{service.s_no}</h3>
                     <h2>{service.s_name}</h2>
                     <p>{service.s_desc}</p>
-                    <div className="services-readmore">
-                        <p>Read More</p>
-                        <img src={arrow_icon} alt="" />
-                    </div>
                 </div>
             })}
         </div>
