8e809540fa24a225fdcd7c196fb83899d6e3c3a7
M-UsmanAli <aliusmanmuhammad98@gmail.com>
2024-05-27 23:12:14 +0500

    SEO of hero content

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import './Hero.css'
-import profile_img from '../../assets/profile_img.svg'
+import React from 'react';
 import AnchorLink from "react-anchor-link-smooth-scroll";
-import profile_pic from '../../assets/profile_pic.png'
+import profile_pic from '../../assets/profile_pic.png';
+import './Hero.css';
 
 const Hero = () => {
   return (
     <div className='hero' id='home'>
         <img src={profile_pic} alt='' className='hero-img'/>
-        <h1><span>I am Muhammad Usman Ali,</span> full stack developer.</h1>
-        <p>As a dedicated React.js and Next.js application developer, I have played a pivotal role in creating cutting-edge and dynamic user interfaces. My expertise extends to the entire software development life cycle, from conceptualization and design to implementation and deployment.</p>
+        <h1><span>I am Muhammad Usman Ali,</span> a Full Stack Developer.</h1>
+        <p>As a full stack developer specializing in React.js and Next.js, I create dynamic user interfaces and responsive web applications. I handle the entire software development life cycle, from design to deployment, ensuring high-quality solutions and optimized performance.</p>
         <div className="hero-action">
             <div className="hero-connect"><AnchorLink className="anchor-link" offset={50} href="#contact">Connect With Me</AnchorLink></div>
             <div className="hero-resume">My Resume</div>
